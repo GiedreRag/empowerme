487b7660c68c66f92fb120e441c1a7184934260a
Giedre <giedre.rageliene@gmail.com>
2024-01-05 14:57:54 +0200

    First stage of message form (front and back)

--- server/dbSetup.js
+++ server/dbSetup.js
@@ -17,7 +17,29 @@ async function dbSetup() {
     await connection.execute(`CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\``);
     connection.query(`USE \`${DB_DATABASE}\``);
 
+    if (database_reset) {
+        await formSubmissionsTable(connection);
+    }
+    
     return connection;
+}
+
+async function formSubmissionsTable(db) {
+    try {
+        const sql = `CREATE TABLE \`form-submissions\` (
+                        id int(100) NOT NULL AUTO_INCREMENT,
+                        name varchar(60) NOT NULL,
+                        email varchar(100) NOT NULL,
+                        message text NOT NULL,
+                        createdAt timestamp NOT NULL DEFAULT current_timestamp(),
+                        PRIMARY KEY (id)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci`;
+        await db.execute(sql);      
+    } catch (error) {
+        console.log("Couldn't create form submissions table.");
+        console.log(error);
+        throw error;
+    }
 }
 
 export const connection = await dbSetup();
