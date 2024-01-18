4d5aa0f606a1641279b95f8771e3054f59445ca2
Giedre <giedre.rageliene@gmail.com>
2024-01-18 12:34:51 +0200

    Burger menu 1st stages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import style from '../components/Header.module.css';
 import { LanguageToggle } from './LanguageToggle';
 import { useLanguage } from '../context/LanguageContext';
+import { FaBars } from 'react-icons/fa';
 
 export function Header() {
     const { language } = useLanguage();
+    const [menuVisible, setMenuVisible] = useState(false);
 
     const translations = require(`../locales/${language}/translation.json`);
 
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    }
+
+    const closeMenu = () => {
+        setMenuVisible(false);
+    };
+
     return (
         <div className={style.headerContainer}>
-            <header className="d-flex flex-wrap justify-content-middle align-items-center mb-4 border-bottom me-2 ms-2">
+            <header className={`d-flex flex-wrap justify-content-middle align-items-center mb-4 border-bottom me-2 ms-2 ${menuVisible ? style.active : ''} ${window.innerWidth <= 790 ? style.smallScreen : ''}`}>
                 <Link to="/" className="mb-md-0 me-md-auto">
                     <img 
                         src="https://media.canva.com/1/image-resize/1/500_500_100_PNG_F/czM6Ly9tZWRpYS1wcml2YXRlLmNhbnZhLmNvbS9vNzVsWS9NQUVfUGRvNzVsWS8xL3AucG5n?osig=AAAAAAAAAAAAAAAAAAAAALMkqn08CI_8u_P4iI9Wia3dLSeR1rXMHFzP_4T8Zc0j&amp;exp=1704199119&amp;x-canva-quality=screen&amp;csig=AAAAAAAAAAAAAAAAAAAAANudEeF02Rgevzm1xaBHT2veiLsZrFlbMH03sWd-ENZH" 
@@ -18,14 +29,69 @@ export function Header() {
                         style={{ width: '110px', height: 'auto'}}
                     />
                 </Link>
-                <ul className="nav nav-pills">
-                    <li className="nav-item"><Link to="/apie-mane" className="nav-link text-body-secondary">{translations.header_about_me}</Link></li>
-                    <li className="nav-item"><Link to="/produktai" className="nav-link text-body-secondary">{translations.header_products}</Link></li>
-                    <li className="nav-item"><Link to="/paketai" className="nav-link text-body-secondary">{translations.header_packages}</Link></li>
-                    <li className="nav-item"><Link to="/susisiekti" className="nav-link text-body-secondary">{translations.header_contact}</Link></li>
-                </ul>
-                <LanguageToggle />
+                <nav className={`${style.headerMenu}`}>
+                    <Link to="/apie-mane" className="nav-link text-body-secondary">{translations.header_about_me}</Link>
+                    <Link to="/produktai" className="nav-link text-body-secondary">{translations.header_products}</Link>
+                    <Link to="/paketai" className="nav-link text-body-secondary">{translations.header_packages}</Link>
+                    <Link to="/susisiekti" className="nav-link text-body-secondary">{translations.header_contact}</Link>
+                </nav>
+                <div className={style.smallScreen}>
+                    <button className={`${style.burgerMenuButton} ${menuVisible ? style.active : ''}`} onClick={toggleMenu}>
+                        <i className="d-flex"><FaBars /></i>
+                    </button>
+                    {menuVisible && (
+                        <nav className={`${style.menuVisible}`}>
+                            <Link to="/apie-mane" className="nav-link text-body-secondary" onClick={closeMenu}>{translations.header_about_me}</Link>
+                            <Link to="/produktai" className="nav-link text-body-secondary" onClick={closeMenu}>{translations.header_products}</Link>
+                            <Link to="/paketai" className="nav-link text-body-secondary" onClick={closeMenu}>{translations.header_packages}</Link>
+                            <Link to="/susisiekti" className="nav-link text-body-secondary" onClick={closeMenu}>{translations.header_contact}</Link>
+                        </nav>
+                    )}
+                    <LanguageToggle />
+                </div>
             </header>
         </div>
     );
 }
+
+
+// import { Link } from "react-router-dom";
+// import { useState } from "react";
+// import style from '../components/Header.module.css';
+// import { LanguageToggle } from './LanguageToggle';
+// import { useLanguage } from '../context/LanguageContext';
+
+// export function Header() {
+//     const { language } = useLanguage();
+//     const [menuVisible, setMenuVisible] = useState(false);
+
+//     const translations = require(`../locales/${language}/translation.json`);
+
+//     const toggleMenu = () => {
+//         setMenuVisible(!menuVisible);
+//     }
+
+//     return (
+//         <div className={style.headerContainer}>
+//             <header className={`d-flex flex-wrap justify-content-middle align-items-center mb-4 border-bottom me-2 ms-2 ${menuVisible ? style.active : ''}`}>
+//                 <Link to="/" className="mb-md-0 me-md-auto">
+//                     <img 
+//                         src="https://media.canva.com/1/image-resize/1/500_500_100_PNG_F/czM6Ly9tZWRpYS1wcml2YXRlLmNhbnZhLmNvbS9vNzVsWS9NQUVfUGRvNzVsWS8xL3AucG5n?osig=AAAAAAAAAAAAAAAAAAAAALMkqn08CI_8u_P4iI9Wia3dLSeR1rXMHFzP_4T8Zc0j&amp;exp=1704199119&amp;x-canva-quality=screen&amp;csig=AAAAAAAAAAAAAAAAAAAAANudEeF02Rgevzm1xaBHT2veiLsZrFlbMH03sWd-ENZH" 
+//                         alt="logo of Monica Empower"
+//                         style={{ width: '110px', height: 'auto'}}
+//                     />
+//                 </Link>
+//                 <button className={`${style.burgerMenuButton} ${menuVisible ? style.active : ''}`} onClick={toggleMenu}>
+//                     MENU
+//                 </button>
+//                 <nav className={`nav nav-pills ${style.navPills}`}>
+//                     <Link to="/apie-mane" className="nav-link text-body-secondary">{translations.header_about_me}</Link>
+//                     <Link to="/produktai" className="nav-link text-body-secondary">{translations.header_products}</Link>
+//                     <Link to="/paketai" className="nav-link text-body-secondary">{translations.header_packages}</Link>
+//                     <Link to="/susisiekti" className="nav-link text-body-secondary">{translations.header_contact}</Link>
+//                 </nav>
+//                 <LanguageToggle />
+//             </header>
+//         </div>
+//     );
+// }
